0583a4ae92b04247ed38f3a14d9351328997028d
Денис Михайлов <63953435+MhlvDenis@users.noreply.github.com>
2023-05-14 23:04:46 +0300

    Delete hw6 directory

--- hw6/cut-sequence.py
+++
@@ -1,6 +0,0 @@
-# >NC_000019.10:c33299900-33299801 Homo sapiens chromosome 19, GRCh38.p14 Primary Assembly
-seq = "AATCTCTGGGAAAACGGCATTAATAACACGAACAATCTGTGTTGGCCGGGGGAACCCAGGAGCCCCAGGCAGGGGTGGGGCTGTGAGGGGAGGGAAGCTG"
-with open('cut-sequences.fasta', 'w') as file:
-    for i in range(100, 0, -1):
-        file.write('>fragment_' + str(i) + '\n')
-        file.write(seq[:i] + '\n')
